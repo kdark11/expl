4614261ce384528cf5e3dad051e7e7f6fb7c17d1
kdark11 <k.dark11@gmil.com>
2023-01-07 22:30:14 -0700

    added form

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from forms import TaskForm
+import secrets
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = TaskForm()
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run()
