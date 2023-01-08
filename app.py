3ba4e41fbbed3ba857c2eb3efb1665000ec87a89
kdark11 <k.dark11@gmil.com>
2023-01-07 22:37:36 -0700

    form can submit now

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from forms import TaskForm
 import secrets
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = TaskForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+        # Save the task to the database
+        flash('Task created successfully')
+        return redirect(url_for('index'))
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
